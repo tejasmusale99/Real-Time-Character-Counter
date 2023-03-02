6de8e89c20ccca0bfd355fc291c0bc21c98d8d4e
Tejas Musale <tejasmusale99@gmail.com>
2023-03-02 21:18:07 +0530

    Done with CSS

---
+++ index.js
@@ -0,0 +1,10 @@
+const inputText=document.getElementById("textarea")
+const totalCounter=document.getElementById("total-counter")
+const remainingCounter=document.getElementById("remaining-counter")
+
+const inputValue=inputText.value.length
+const totalCharc=totalCounter.innerText=inputValue
+const remainingCharc=remainingCounter.innerText
+console.log(totalCharc,remainingCharc);
+
+
