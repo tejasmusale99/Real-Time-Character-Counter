caf3fd9c4d46a2dda27e71218c961637506f8b25
Tejas Musale <tejasmusale99@gmail.com>
2023-03-03 10:13:53 +0530

    Read input length

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const inputText=document.getElementById("textarea")
-const totalCounter=document.getElementById("total-counter")
-const remainingCounter=document.getElementById("remaining-counter")
+// const totalCounter=document.getElementById("total-counter")
+// const remainingCounter=document.getElementById("remaining-counter")
+function inputHandler(){
+    const inputValue=inputText.value.length
+    console.clear()
+    console.log(inputValue)
+}
+inputText.addEventListener("input",inputHandler)
 
-const inputValue=inputText.value.length
-const totalCharc=totalCounter.innerText=inputValue
-const remainingCharc=remainingCounter.innerText
-console.log(totalCharc,remainingCharc);
+
 
 
