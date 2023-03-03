9a6ca61b907c0cbf185a14b104487e96ec2606a7
Tejas Musale <tejasmusale99@gmail.com>
2023-03-03 11:06:19 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const totalCounter=document.getElementById("total-counter")
 const remainingCounter=document.getElementById("remaining-counter")
 
 function inputHandler(){
-    totalCounter.innerText=inputText.value.length
+   totalCounter.innerText=inputText.value.length
+  remainingCounter.innerText=inputText.getAttribute("maxLength") - inputText.value.length
 }
 inputText.addEventListener("keyup",inputHandler)
 
